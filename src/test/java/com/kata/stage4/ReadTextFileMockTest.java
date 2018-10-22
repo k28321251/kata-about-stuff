b7b795c15100cdca2d2e00a93ad40d87b7c0e4bc
Nirmal Jacob <Nirmal.Jacob@wolterskluwer.com>
2018-10-21 22:10:27 -0400

    Added couple of packages needed for the Kata

---
+++ src/test/java/com/kata/stage4/ReadTextFileMockTest.java
@@ -0,0 +1,9 @@
+package com.kata.stage4;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public final class ReadTextFileMockTest {
+
+}
