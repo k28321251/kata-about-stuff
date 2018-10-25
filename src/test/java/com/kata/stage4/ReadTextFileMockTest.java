75c17a081e8cf9af399e27f5514e31a49dbc30ee
Nirmal Jacob <Nirmal.Jacob@wolterskluwer.com>
2018-10-25 12:49:39 -0400

    Fixed the broken build

--- src/test/java/com/kata/stage4/ReadTextFileMockTest.java
+++ src/test/java/com/kata/stage4/ReadTextFileMockTest.java
@@ -1,9 +1,5 @@
 package com.kata.stage4;
 
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
-
-@RunWith(MockitoJUnitRunner.class)
 public final class ReadTextFileMockTest {
 
 }
